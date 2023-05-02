70983b354c003192ef90ac83f66a0fcd074154bc
An Na <99172832+An0401na@users.noreply.github.com>
2023-05-02 20:18:43 +0900

    impl: RB Tree  삽입, 삭제 일부

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -5,24 +5,44 @@ public class Node implements iNode{
     Node right;
     int color;
     boolean hasExtraBlack;
-    
+
     public Node() {
-        
+
         // default value
         color = RED;
         hasExtraBlack = false;
     }
-    
+
     @Override
     public int findBrotherColor() {
         return 0;
     }
-    
+
+
+    /**
+     * 현재 노드가 부모의 어느쪽 자식인지 확인합니다.
+     * @return 왼쪽 자식 : -1, 오른쪽 자식 : 1, root Node(부모 X) : 0
+     */
     @Override
     public int findSide() {
+        //현 노드가 부모가 없는 루트노드
+        if(parent == null) {
+            return 0;
+        }
+
+        //부모의 왼쪽자식이 현재 노드
+        if(parent.left.value == value){
+            return -1;
+        }
+
+        //부모의 오른쪽 자식이 현재 노드
+        if(parent.right.value == value){
+            return 1;
+        }
+
         return 0;
     }
-    
+
     @Override
     public int hasRedChild() {
         return 0;
--- src/main/java/RedBlackTree.java
+++ src/main/java/RedBlackTree.java
@@ -1,44 +1,195 @@
 public class RedBlackTree implements iRedBlackTree{
+
+    static final int BLACK = -1;
+    static final int RED = 1;
+    static Node NIL= new Node();
     Node root;
     int size;
-    
     @Override
     public boolean isEmpty() {
+        if(root == null){
+            return true;
+        }
         return false;
     }
-    
+
     @Override
     public int size() {
-        return 0;
+        return size;
     }
-    
+
     @Override
     public void insert(int value) {
-    
+        if(root == null){
+            root = new Node();
+            root.value = value;
+            root.color = BLACK;
+            root.right = NIL;
+            root.left = NIL;
+
+            size ++;
+        }else{
+            insertNode(root, value);
+        }
     }
-    
+
+    private void insertNode(Node node, int value) {
+
+        //삽입과정
+        if(value < node.value){
+            if(node.left == NIL){
+                node.left = new Node();
+                Node newNode = node.left;
+                newNode.value = value;
+                newNode.color = RED;
+                newNode.right = NIL;
+                newNode.left = NIL;
+                newNode.parent = node;
+                size++;
+            }else{
+                insertNode(node.left, value);
+            }
+        }else if(value > node.value){
+            if(node.right == NIL){
+                node.right = new Node();
+                Node newNode = node.right;
+                newNode.value = value;
+                newNode.color = RED;
+                newNode.right = NIL;
+                newNode.left = NIL;
+                newNode.parent = node;
+                size++;
+            }else{
+                insertNode(node.right, value);
+            }
+        }else{
+//            throw new CustomDuplicatedElementException();
+        }
+
+        if(node.color == RED){
+            /*/**
+             * 현재 노드가 부모의 어느쪽 자식인지 확인합니다.
+             * @return 왼쪽 자식 : -1, 오른쪽 자식 : 1, root Node(부모 X) : 0
+            int findSide();
+            */
+            //LL인 경우
+            if(node.left.value == value){
+                if(node.findSide() == -1){
+                    // LL인 경우
+                } else if (node.findSide() == 1) {
+                    // LR인 경우
+                    if( ){
+
+                    }
+
+                }
+            }
+
+        }
+
+
+
+
+    }
+
     @Override
     public boolean contains(int value) {
-        return false;
+        return containsNode(root, value);
     }
-    
+
+    private boolean containsNode(Node node, int value) {
+        if(node == null) {
+            return false;
+        }
+
+        if(node.value == value){
+            return true;
+        }
+
+        if(value < node.value){
+            return containsNode(node.left, value);
+        }else{
+            return containsNode(node.right, value);
+        }
+    }
+
     @Override
     public void delete(int value) {
-    
+        //루트 삭제시 null로 바꾸기
+
     }
-    
+    //RR인 경우
+    /*     node           	   R
+               R     =>  node     RR
+     * 		 RL RR          RL
+     */
     @Override
     public void rotateLeft(Node tree) {
-    
+        if(tree == root) {
+            root = tree.right;
+        }
+
+        Node R = tree.right;
+        Node RL = tree.right.left;
+
+        if(tree.parent != null) {
+            tree.parent.right = R;
+        }
+        R.parent = tree.parent;
+
+        tree.parent = R;
+        tree.right = RL;
+        R.left = tree;
+
+        if(RL != null) {
+            RL.parent = tree;
+        }
     }
-    
+
+    //LL인 경우
+    /*     node           L
+          L       =>  LL    node
+     *  LL LR              LR
+     */
     @Override
     public void rotateRight(Node tree) {
-    
+        if(tree == root) {
+            root = tree.left;
+        }
+
+        Node L = tree.left;
+        Node LR = tree.left.right;
+
+        if(tree.parent != null) {
+            tree.parent.left = L;
+        }
+        L.parent = tree.parent;
+
+        tree.parent = L;
+        tree.left = LR;
+        L.right = tree;
+
+        if(LR != null) {
+            LR.parent = tree;
+        }
     }
-    
+    //subtree의 black 수 계산 (자기 자신 제외 + nil 노드 포함)
     @Override
     public int countBlack(Node tree) {
-        return 0;
+        int count =0;
+
+        while (true){
+            if(tree == NIL){
+                count ++;
+                break;
+            }
+            if(tree.right.color == BLACK) {
+                count++;
+            }
+            tree= tree.right;
+        }
+
+        return count;
+
     }
 }
