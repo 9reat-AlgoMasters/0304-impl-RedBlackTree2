b8df94dd4b421f6355f2fdf92daac431b9f9dcfe
itsdangerous <danger121@naver.com>
2023-05-19 01:09:49 +0900

    test: insert 메서드 테스트 코드 추가

--- src/test/java/RedBlackTreeTest.java
+++ src/test/java/RedBlackTreeTest.java
@@ -1,12 +1,36 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 class RedBlackTreeTest {
     @Test
     void simpleTest() {
         RedBlackTree tree = new RedBlackTree();
-        assertThat(tree.isEmpty()).isFalse();
+        assertThat(tree.isEmpty()).isTrue();
+    }
+
+    @Test
+    @DisplayName("빈 트리에 원소를 추가하는 경우")
+    void insertIntoEmptyTree() {
+        RedBlackTree tree = new RedBlackTree();
+        tree.insert(5);
+
+        assertThat(tree.root.value).isEqualTo(5);
+        assertThat(tree.root.color).isEqualTo(iNode.BLACK);
+    }
+
+    @Test
+    @DisplayName("하나의 원소가 있는 트리에 원소를 추가하는 경우")
+
+    void insertIntoTreeWithOneElement() {
+        RedBlackTree tree = new RedBlackTree();
+        tree.insert(5);
+        tree.insert(3);
+
+        assertThat(tree.root.value).isEqualTo(5);
+        assertThat(tree.root.color).isEqualTo(iNode.BLACK);
+        assertThat(tree.root.left.value).isEqualTo(3);
+        assertThat(tree.root.left.color).isEqualTo(iNode.RED);
     }
 }
