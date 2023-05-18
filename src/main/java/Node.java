4dac9c9275e725593d8b6059b0e770aeea35d810
itsdangerous <danger121@naver.com>
2023-05-19 01:07:47 +0900

    impl: Node 클래스 구현

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -5,26 +5,54 @@ public class Node implements iNode{
     Node right;
     int color;
     boolean hasExtraBlack;
-    
     public Node() {
+
+        // default value
+        this.color = RED;
+        this.hasExtraBlack = false;
+    }
+    
+    public Node(int value) {
         
         // default value
-        color = RED;
-        hasExtraBlack = false;
+        this.color = RED;
+        this.hasExtraBlack = false;
+        this.value = value;
     }
     
     @Override
     public int findBrotherColor() {
-        return 0;
+        if (parent == null)
+            return 0;  // 형제 노드 없음
+
+        Node brother = (this == parent.left) ? parent.right : parent.left;
+
+        if (brother != null)
+            return brother.color;
+
+        return 0;  // 형제 노드 없음
     }
     
     @Override
     public int findSide() {
-        return 0;
+        if (parent == null)
+            return 0;  // root node
+
+        return (this == parent.left) ? -1 : 1;  // -1: 왼쪽 자식, 1: 오른쪽 자식
     }
     
     @Override
     public int hasRedChild() {
-        return 0;
+        int leftColor = (left != null) ? left.color : BLACK;
+        int rightColor = (right != null) ? right.color : BLACK;
+
+        if (leftColor == RED && rightColor == RED)
+            return 2;  // 양쪽 자식이 모두 RED
+        else if (leftColor == RED)
+            return -1;  // 왼쪽 자식만 RED
+        else if (rightColor == RED)
+            return 1;  // 오른쪽 자식만 RED
+
+        return 0;  // 양쪽 자식이 모두 RED가 아님
     }
 }
